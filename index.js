92d6bbc196876461715b31e06f2e5b7832a61c3c
Alexandre <alexandrepj13@gmail.com>
2022-05-31 20:32:32 -0300

    Colocando opção de check para ás tarefas

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ document.getElementById("btn-cadastrar").addEventListener("click",()=>{
     let element = document.createElement("li");
     
     element.innerHTML=`
-    ${nome}
-
+    <input type="checkbox" id="label">
+    <label class="label-check" for="label">${nome}</label>
     `
     let remove = document.createElement("button");
     remove.type = "button";
     remove.textContent = "X"
     remove.setAttribute("onclick","removerItem(this)");
     remove.style.width ="30px"
+    remove.style.position = "absolute";
+    remove.style.right ="1rem"
+    remove.style.top="2.5rem"
     remove.style.border = "none"
     remove.style.backgroundColor = "#000"
     remove.style.color = "red"
